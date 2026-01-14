3bdefecf3dc800978cd275513d4fc0294e2d4752
Maximilian Kaden <maximiliank94@users.noreply.github.com>
2026-01-15 00:00:55 +0100

    Home Page Preview

--- web/src/app/pages/app/home/home.page.ts
+++ web/src/app/pages/app/home/home.page.ts
@@ -1,5 +1,5 @@
-import {CommonModule} from '@angular/common';
-import {Component, inject, signal} from '@angular/core';
+import {CommonModule, isPlatformBrowser} from '@angular/common';
+import {ChangeDetectorRef, Component, inject, PLATFORM_ID, signal} from '@angular/core';
 import {ReactiveFormsModule} from '@angular/forms';
 import {Router} from '@angular/router';
 import {PageHeader} from '../../../components/page-header/page-header';
@@ -9,6 +9,8 @@ import {LINK} from '../../../app.routes';
 import {Dialog} from 'primeng/dialog';
 import {Button, ButtonIcon, ButtonLabel} from 'primeng/button';
 import {CreateExamForm} from '../../../components/create-exam-form/create-exam-form';
+import {TableModule} from 'primeng/table';
+import {Badge} from 'primeng/badge';
 
 @Component({
   selector: 'app-home-page',
@@ -22,7 +24,9 @@ import {CreateExamForm} from '../../../components/create-exam-form/create-exam-f
     Button,
     CreateExamForm,
     ButtonLabel,
-    ButtonIcon
+    ButtonIcon,
+    TableModule,
+    Badge
   ],
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
@@ -30,9 +34,90 @@ import {CreateExamForm} from '../../../components/create-exam-form/create-exam-f
 export class HomePage {
   private router = inject(Router);
   protected readonly faPlay = faPlay;
-
-
   visible = signal(false);
+
+  basicData: any;
+
+  basicOptions: any;
+
+  platformId = inject(PLATFORM_ID);
+
+  constructor(private cd: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    this.initChart();
+  }
+
+  initChart() {
+    if (isPlatformBrowser(this.platformId)) {
+      const documentStyle = getComputedStyle(document.documentElement);
+      const textColor = documentStyle.getPropertyValue('--p-text-color');
+      const textColorSecondary = documentStyle.getPropertyValue('--p-text-muted-color');
+      const surfaceBorder = documentStyle.getPropertyValue('--p-content-border-color');
+
+      this.basicData = {
+        labels: [
+          "Fundamentals & Architecture",
+          "Reactivity & State",
+          "Performance & Rendering",
+          "Navigation & UX (Router)",
+          "Forms & Complex UI",
+          "Platform & Quality (Build/Tests)",
+          "Architecture & Scale (SSR/Security)"
+        ],
+        datasets: [
+          {
+            type: 'line',
+            label: 'Average',
+            data: [5, 6, 4, 6, 7, 5, 3],
+            backgroundColor: documentStyle.getPropertyValue('--p-blue-500'),
+            borderColor: documentStyle.getPropertyValue('--p-blue-500'),
+            tension: 0.4,
+          },
+          {
+            label: 'Your Current State',
+            data: [4, 5, 3, 4, 6, 3, 1],
+            backgroundColor: documentStyle.getPropertyValue('--p-green-500'),
+          },
+          {
+            label: 'Senior',
+            data: [8, 8, 7, 9, 10, 7, 5],
+            backgroundColor: documentStyle.getPropertyValue('--p-pink-500'),
+          }
+        ],
+      };
+
+      this.basicOptions = {
+        plugins: {
+          legend: {
+            labels: {
+              color: textColor,
+            },
+          },
+        },
+        scales: {
+          x: {
+            ticks: {
+              color: textColorSecondary,
+            },
+            grid: {
+              color: surfaceBorder,
+            },
+          },
+          y: {
+            beginAtZero: true,
+            ticks: {
+              color: textColorSecondary,
+            },
+            grid: {
+              color: surfaceBorder,
+            },
+          },
+        },
+      };
+      this.cd.markForCheck()
+    }
+  }
 
   showDialog() {
     this.visible.set(true);
@@ -40,5 +125,37 @@ export class HomePage {
 
   redirectToExam(examId: string) {
     this.router.navigate(LINK.exam(examId)).then(() => {});
+  }
+
+  products = [
+    {
+      id: '1000',
+      code: 'Fundamentals & Architecture',
+      name: 'Mid Level +',
+      description: 'Product Description',
+      image: 'bamboo-watch.jpg',
+      price: 65,
+      category: 'Senior',
+      quantity: 24,
+      inventoryStatus: 'INSTOCK',
+      rating: 5
+    }]
+
+  rowClass(product: any) {
+    return { '!bg-primary !text-primary-contrast': product.category === 'Fitness' };
+  }
+
+  rowStyle(product: any) {
+    if (product.quantity === 0) {
+      return { fontWeight: 'bold', fontStyle: 'italic' };
+    }
+
+    return { fontWeight: 'normal', fontStyle: 'normal' };
+  }
+
+  stockSeverity(product: any) {
+    if (product.quantity === 0) return 'danger';
+    else if (product.quantity > 0 && product.quantity < 10) return 'warn';
+    else return 'success';
   }
 }
