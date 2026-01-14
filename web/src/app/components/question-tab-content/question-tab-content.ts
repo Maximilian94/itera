efe93aa32580c13a224d6f0b926e4fba9d50e8e2
Maximilian Kaden <maximiliank94@users.noreply.github.com>
2026-01-14 13:42:13 +0100

    Question tab - next and previous buttons

--- web/src/app/components/question-tab-content/question-tab-content.ts
+++ web/src/app/components/question-tab-content/question-tab-content.ts
@@ -5,25 +5,34 @@ import type {Question} from '../../api/api.types';
 import { ExamInExecution } from '../../services/exam/ui/adapters/exam.adapter';
 import { AttemptsInProgressResponse, AttemptsResponse } from '../../services/exam/domain/exam.interface';
 import {ExamOptionCardDirective} from '../../services/exam/ui/directives/exam-option-card.directive';
+import {FaIconComponent} from '@fortawesome/angular-fontawesome';
+import {faChevronLeft, faChevronRight} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-question-tab-content',
   standalone: true,
-  imports: [CommonModule, ButtonModule, ExamOptionCardDirective],
+  imports: [CommonModule, ButtonModule, ExamOptionCardDirective, FaIconComponent],
   templateUrl: './question-tab-content.html',
   styleUrl: './question-tab-content.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class QuestionTabContent {
+  protected readonly faChevronRight = faChevronRight;
+  protected readonly faChevronLeft = faChevronLeft;
+  protected readonly alphabet:string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
   @Input({ required: true }) question!: Question;
   @Input({ required: true }) selectedOptionId!: string | null;
   @Input() disableSelection: boolean = false;
   @Input({ required: true }) examStatus!: ExamInExecution["exam"]["status"];
   @Input({ required: true }) attempt!: AttemptsResponse | AttemptsInProgressResponse;
+  @Input() disablePrev = false;
+  @Input() disableNext = false;
+
   @Output() optionSelected = new EventEmitter<string>();
   @Output() uncheckedOption = new EventEmitter<void>();
-
-  protected alphabet:string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  @Output() prev = new EventEmitter<void>();
+  @Output() next = new EventEmitter<void>();
 
   // When user clicks the already-selected option, emit null to "unselect".
   onButtonClick(optionId: string): void {
@@ -37,5 +46,3 @@ export class QuestionTabContent {
     this.optionSelected.emit(optionId);
   }
 }
-
-
