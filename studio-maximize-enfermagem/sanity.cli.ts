1639dd25c56f960c636c3df6b8a72c779b5f8e28
Maximilian Kaden <maximiliank94@users.noreply.github.com>
2026-02-19 11:20:59 +0100

    WP

---
+++ studio-maximize-enfermagem/sanity.cli.ts
@@ -0,0 +1,11 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'p0zbvdyp',
+    dataset: 'production',
+  },
+  deployment: {
+    appId: 'm3kl3bl43ir6ix5dz5z4mzdv',
+  },
+})
